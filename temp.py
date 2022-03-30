ed9fc73d1cf77528f9869d5eb4d43c2166ed6f37
judylei99 <qlei1@uchicago.edu>
2022-03-30 14:16:10 -0500

    testing

---
+++ temp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+print('Hello World')
+x = 10
+print(x*2)
+
+print('History of files can be seen now on Github?')
