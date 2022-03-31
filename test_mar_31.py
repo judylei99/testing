f3f7ae8e885e1bfde8079ead4bd34c6b5ec4ce2d
judylei99 <qlei1@uchicago.edu>
2022-03-31 11:01:23 -0500

    For course

---
+++ test_mar_31.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+print('Hello World')
+x = 10
+print(x*2)
+print('can this still change?')
+print('This is a test on Mar 31st')
